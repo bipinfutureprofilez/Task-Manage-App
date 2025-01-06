02fdf919a8332afa5e09b0d998eacd486acd4910
bipinfutureprofilez <bipinkr7742@gmail.com>
2025-01-06 22:13:17 +0530

    created README.md file

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Homepage() {
   return (
       <div className="text-center">
-          <h1 className="text-4xl font-bold mb-7 text-white ">Welcome to Task Management App</h1>
+          <h1 className=" text-2xl md:text-3xl lg:text-4xl leading-[100%] font-semibold mb-4 md:mb-7 text-white ">Welcome to Task Management App</h1>
           <Link to="/task-manager">
               <button className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600">Go to Task Manager</button>
           </Link>
--- src/Pages/TaskManager.jsx
+++ src/Pages/TaskManager.jsx
@@ -21,13 +21,13 @@ function TaskManager() {
 
     return (
         <>
-        <div className="w-full p-6 bg-white shadow-lg rounded-md max-w-4xl mx-auto mt-10 relative ">
+        <div className="w-full p-6 bg-white shadow-lg rounded-md max-w-4xl mx-auto relative ">
             <Link to='/' className='text-sm absolute top-[10px] left-0 bg-black text-white py-[7px] px-[15px]' style={{borderRadius: '0 45px 45px 0'}}>Back</Link>
             <div className='flex flex-col md:flex-row items-end gap-[40px]'>
-                <div className='w-full md:w-1/2 flex justify-center items-center pl-[50px]'>
+                <div className='w-full md:w-1/2 hidden md:flex justify-center items-center pl-[50px]'>
                     <img src="assets/img/task-img.png" alt="img" className='max-w-[100%] w-full object-contain' />
                 </div>
-                <div className='w-full md:w-1/2'>
+                <div className='w-full md:w-1/2 pt-8 md:pt-0'>
                     <h2 className="text-xl font-semibold mb-4">Task Manager</h2>
                     <TaskForm addTask={addTask} editTask={editTask} taskToEdit={taskToEdit} setTaskToEdit={setTaskToEdit} />
                 </div>
